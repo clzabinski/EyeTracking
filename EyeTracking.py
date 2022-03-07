15a55d4be12c0cf72bab4996d0ca98a655c15923
Casper Norspang <norspang@pop-os.localdomain>
2022-03-07 09:52:07 +0100

    Added spacebar for taking datapoints

--- EyeTracking.py
+++ EyeTracking.py
@@ -4,10 +4,15 @@ Check the README.md for complete documentation.
 """
 
 import cv2
+import time
+from keyboard import is_pressed
 from gaze_tracking import GazeTracking
+from DataLib import take_point
 
 gaze = GazeTracking()
 webcam = cv2.VideoCapture(0)
+
+take_info = True
 
 while True:
     # We get a new frame from the webcam
@@ -32,10 +37,21 @@ while True:
 
     left_pupil = gaze.pupil_left_coords()
     right_pupil = gaze.pupil_right_coords()
+    horizontal_value = gaze.horizontal_ratio()
+    vertical_value = gaze.vertical_ratio()
     cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
     cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    cv2.putText(frame, "Horizontal Value: " + str(horizontal_value), (90, 200), cv2.FONT_HERSHEY_DUPLEX, 0.9,
+                (147, 58, 31), 1)
+    cv2.putText(frame, "Vertical Value: " + str(vertical_value), (90, 235), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31),
+                1)
 
     cv2.imshow("Demo", frame)
+
+    if is_pressed(' '):
+        for i in range(20):
+            take_point(left_pupil, right_pupil, horizontal_value, vertical_value, text)
+            time.sleep(0.1)
 
     if cv2.waitKey(1) == 27:
         break
