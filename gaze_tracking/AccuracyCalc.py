bca90d515c08a1e84742c8cc32e91823f6a952f4
ChristofferAAU <cn19@student.aau.dk>
2022-03-24 10:52:11 +0100

    Updated the calcaccuracy methods, and the calcaccuracyavelist

--- gaze_tracking/AccuracyCalc.py
+++ gaze_tracking/AccuracyCalc.py
@@ -2,23 +2,23 @@ import math
 
 class Accuracy():
 
-    def calcaccuracy(self, point, d):#Make this use a point class
+    def calcaccuracy(self, point, base, d):#Make this use a point class
 
-        theta = math.atan(math.sqrt(math.pow()-math.pow())/d)
+        theta = math.atan(math.sqrt(math.pow(point.x-base.x)+math.pow(point.y-base.y))/d)
 
         return theta
 
-    def calcaccuracylist(self, list, d):
+    def calcaccuracylist(self, list, base, d):
         result = [1]
         for x in list:
-            result.append(math.atan(math.sqrt(math.pow()-math.pow())/d))
+            result.append(math.atan(math.sqrt(math.pow(x.x-base.x)-math.pow(x.y-base.y))/d))
 
-        return
+        return result
 
-    def calcaccuracyave(self):
+    def calcaccuracyavelist(self, list):
+        result = 0
+        for x in list:
+           result = x + result
 
-        return
-
-    def calcaccuracyavelist(self):
-
-        return
+        result = result/list.count
+        return result
