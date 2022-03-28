3373f900febf849331b12f968a8ae19191976654
norspang <norspang@pop-os.localdomain>
2022-03-28 15:48:49 +0200

    Data nd accuracy

--- Accuracy.py
+++ Accuracy.py
@@ -3,23 +3,23 @@ import math
 
 class Accuracy:
     def __init__(self):
-        self.average = accuracy_average()
-        self.theta = calc_accuracy(type(int))
-        self.accuracy_list = calc_accuracy(type(list))
+        self.average = self.accuracy_average()
+        self.accuracy_list = self.calc_accuracy()
 
+    def accuracy_average(self):
+        sum = 0
+        for x in self.accuracy_list:
+            sum += x
+        average = math.sqrt(sum)
+        return average
 
-def accuracy_average(self):
-    sum = 0
-    for x in self.accuracy_list():
-        sum += self.accuracy_list(x)
-    average = math.sqrt(sum)
-    return average
-
-
-def calc_accuracy(
-    self,
-):  # This should make use of the data points gathered by Mr. North
-    result = []
-    theta = math.atan(math.sqrt(math.pow() - math.pow()) / d)
-    result.append(theta)
-    return theta, result
+    # This should make use of the data points gathered by Mr. North
+    def calc_accuracy(
+        self,
+    ):
+        result = []
+        # d = placeholder for screen distance
+        # xi, xj, yi, yj = coords from points
+        theta = math.atan(math.sqrt(math.pow(xi - xj) - math.pow(yi - yj)) / d)
+        result.append(theta)
+        return result
--- Precision.py
+++ Precision.py
@@ -1,16 +1,13 @@
 from cmath import sqrt
-from Accuracy import accuracy_average
-from Accuracy import calc_accuracy
 
 
 class Precision:
-    def __init__(self):
-        self.r = self.precision(calc_accuracy(type(list)),accuracy_average())
+    def __init__(self, accuracy_list, average):
+        self.r = self.__precision__(accuracy_list, average)
 
-
-def precision(calc_accuracy(type(list)), accuracy_average()):
-    r = 0
-    n = 0
-    for n in range(calc_accuracy(type(list))):
-        r += n * (calc_accuracy(type(list))[:n] - accuracy_average())^2
-    return sqrt(r*1/n)
+    def __precision__(self, accuracy_list, average):
+        r = 0
+        n = len(accuracy_list)
+        for i in range(len(accuracy_list)):
+            r += i * (i - average) ^ 2
+        return sqrt(r * 1 / n)
